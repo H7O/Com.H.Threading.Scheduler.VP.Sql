fcf3d4a64d9431c508071508191f66b80035b3e5
Hussein Al Bayati <hussein@albayati.com>
2021-01-03 12:06:34 +0400

    Add project files.

---
+++ Com.H.Threading.Scheduler.VP.Sql/SqlValueProcessors.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Com.H.Data.EF.Relational;
+using Com.H.Threading.Scheduler.VP;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace Com.H.Threading.Scheduler.VP.Sql
+{
+    public static class SqlValueProcessors
+    {
+
+        #region DbContext
+        private static DbContext GetDbContext(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+            return new DbContext(optionsBuilder.Options);
+        }
+        #endregion
+
+        public static ValueProcessorItem SqlProcessor(this ValueProcessorItem valueItem, 
+            CancellationToken? token = null)
+        {
+            if (valueItem.IsValid("sql") == false) return valueItem;
+            valueItem.Value ??= valueItem.Item.RawValue;
+            if (string.IsNullOrWhiteSpace(valueItem.Item.Attributes?["connection_string"]))
+                throw new MissingFieldException(
+                    $"Missing 'connection_string' attribute for tag '{valueItem.Item.FullName}");
+            try
+            {
+                DbContext db = GetDbContext(valueItem.Item.Attributes?["connection_string"]);
+                valueItem.Data = token == null?
+                    db.ExecuteQuery(valueItem.Value, null)
+                    : Cancellable.CancellableRun<IEnumerable<dynamic>>(
+                    () => db.ExecuteQuery(valueItem.Value, null),
+                    (CancellationToken)token);
+                return valueItem;
+            }
+            catch (Exception ex)
+            {
+                throw new FormatException($"SQL Error executing query for tag {valueItem.Item.FullName}:\r\n"
+                    + $"SQL query:\r\n{valueItem.Value}\r\n\r\n"
+                    + $"Error msg: \r\n{ex.Message}");
+            }
+
+        }
+
+    }
+}
